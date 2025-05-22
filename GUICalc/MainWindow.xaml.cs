02bb3240acd56dc3c20f542fede4dc1f02c5f605
Kegnar <mitriy.exe@gmail.com>
2025-05-22 22:50:50 +0400

    Final

--- GUICalc/MainWindow.xaml.cs
+++ GUICalc/MainWindow.xaml.cs
@@ -1,14 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+﻿using System.Windows;
 namespace GUICalc;
 
 /// <summary>
@@ -19,5 +9,49 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        OperationSelect.ItemsSource = new List<string>()
+        {
+            "Сложение",
+            "Вычитание",
+            "Умножение",
+            "Деление"
+        };
+       
+    }
+    
+    private void CalculateResult(object sender, RoutedEventArgs e)
+    {
+        if (String.IsNullOrWhiteSpace(FirstParam.Text) || String.IsNullOrWhiteSpace(SecondParam.Text))
+        {
+            MessageBox.Show("Пустое поле для ввода", "Ошибка");
+            return;
+        }
+        var checkFirstParamParse = double.TryParse(FirstParam.Text, out double firstParam);
+        var checkSecondParamParse = double.TryParse(SecondParam.Text, out double secondParam);
+        if (!(checkFirstParamParse & checkSecondParamParse))
+        {
+            MessageBox.Show("Ошибка в данных", "Ошибка");
+            return;
+        }
+        Result.Text = Calc(firstParam, secondParam).ToString();
+    }
+
+    private double Calc(double firstParam, double secondParam)
+    {
+        switch (OperationSelect.SelectedIndex)
+        {
+            case 0:
+                return firstParam + secondParam;
+            case 1:
+                return firstParam - secondParam;
+            case 2:
+                return firstParam * secondParam;
+            case 3:
+                return firstParam / secondParam;
+            default:
+                return Double.NaN; // заглушка - без нее компилятор ругается на отсутствие return-а
+        }
     }
 }
+        
+   
